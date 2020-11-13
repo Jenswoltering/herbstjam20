24aad7b816d8712fc7e91e1f65d0db57590f18aa
Jens Woltering <jens@screenable.io>
2020-11-13 18:22:43 +0100

    change game resolution

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -3,7 +3,7 @@ import MainScene from './scenes/mainScene'
 import PreloadScene from './scenes/preloadScene'
 
 const DEFAULT_WIDTH = 1920
-const DEFAULT_HEIGHT = 1280
+const DEFAULT_HEIGHT = 1080
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
